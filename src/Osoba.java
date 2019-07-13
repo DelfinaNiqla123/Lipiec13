fb81b857cc6dc7a375ef7c5c0f6bdcbf4d727edb
DelfinaNiqla123 <domisia.libra@gmail.com>
2019-07-13 10:52:42 +0200

    pierwsza wersja

---
+++ src/Osoba.java
@@ -0,0 +1,16 @@
+public class Osoba {
+
+    //* Stwórz model: Osoba, Portfel, Pieniadze
+    //* Pieniadze mają kwotę i walutę
+    //* Portfel może zawierać wiele różnych walut
+    //* Osoba płaci używając portfela
+    //* Zaimplementuj sposób płatności jednej osoby do drugiej.
+
+
+
+
+
+
+
+
+}
